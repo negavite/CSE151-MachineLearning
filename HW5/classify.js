c155afff6abf2b57a4e4a355bd645ab1a3fb9cd8
Daniel Kao <dkao@diplateevo.com>
2016-02-28 00:50:16 -0800

    hw5 string kernel classifier

--- HW5/classify.js
+++ HW5/classify.js
@@ -37,3 +37,140 @@ const testingFile = 'hw5test.txt';
  *
  **/
 
+// Load the data from the training file
+console.log('Loading Data...');
+
+// load math.js
+var math = require('mathjs');
+var fs = require('fs');
+
+// Load Training Data
+var trainingArray = loadDataFromFile(trainingFile);
+console.log('Training Data elements: ' + trainingArray.length + " dimension: " + trainingArray[0].length);
+
+// Load Test Data
+var testArray = loadDataFromFile(testingFile);
+console.log('Test Data elements: ' + testArray.length + " dimension: " + testArray[0].length + '\n');
+
+var hyperplane = kernelPerceptron(trainingArray, 1, 3);
+console.log('Testing Perceptron with training data...');
+testHyperplane(trainingArray, hyperplane, 3);
+console.log('Testing Perceptron with test data...');
+testHyperplane(testArray, hyperplane, 3);
+console.log();
+
+hyperplane = kernelPerceptron(trainingArray, 1, 4);
+console.log('Testing Perceptron with training data...');
+testHyperplane(trainingArray, hyperplane, 4);
+console.log('Testing Perceptron with test data...');
+testHyperplane(testArray, hyperplane, 4);
+
+/////////////////////////////////////////////////////////////////////
+// Helper Functions
+/////////////////////////////////////////////////////////////////////
+
+/**
+ * Loads the data from the specified file
+ *
+ */
+function loadDataFromFile(file) {
+	var arr = fs.readFileSync(file).toString().split('\n');	
+	arr = arr.slice(0, arr.length - 1);	
+	for(var i in arr) {
+		arr[i] = arr[i].split('  ');
+	}
+	return arr;
+}
+
+/**
+ * The kernel function used to determine how many substrings of length
+ * are common to both strings.
+ */
+function stringKernel(a, b, length) {
+	var searched = [];
+	var found = 0;
+
+	for(var i = 0; i < a.length - length; i++) {
+		
+		var searchString = a.substring(i, i + length);
+		if(searched.indexOf(searchString) < 0) {
+			if(b.indexOf(searchString) >= 0) {
+				found++;
+			}
+
+			searched.concat([searchString]);
+		}
+	}
+
+	return found;
+}
+
+/**
+ * Perceptron runs the perceptron algorithm on the vectors provided in 
+ * vectors niter times.
+ *
+ */
+function kernelPerceptron(vectors, niter, p) {
+
+	console.log('Building Hyperplane by running perceptron algorithm ' + niter + ' times for p = ' + p + '...');
+
+	var hyperplane = [];
+
+	// If niter is not set, set it to 1
+	if(!niter) {
+		niter = 1;
+	}
+
+	// Run the perceptron algorithm niter times.
+	for (var i = 0; i < niter; i++) {
+
+		// Go through all of the vectors
+		for(var j = 0; j < vectors.length; j++) {
+
+			// Get current line
+			var currValidatingLine = vectors[j];
+			var expected = parseInt(currValidatingLine[currValidatingLine.length - 1]);
+			currValidatingLine = currValidatingLine[0];
+
+			if(expected * calculateKernel(currValidatingLine, hyperplane, p) <= 0) {
+				hyperplane = hyperplane.concat([{y: expected, x: currValidatingLine}]);
+			}
+		}
+	}
+
+	return hyperplane;
+}
+
+/**
+ * Calculates the results of the kernel using the kernel trick
+ */
+function calculateKernel(string, hyperplane, p) {
+
+	var res = 0;
+
+	for(var i = 0; i < hyperplane.length; i++) {
+		res += hyperplane[i].y * stringKernel(hyperplane[i].x, string, p);
+	}
+
+	return res;
+}
+
+function testHyperplane(vectors, hyperplane, p) {
+
+	var error = 0;
+
+	// Go through all of the vectors
+	for(var j = 0; j < vectors.length; j++) {
+
+		// Get current line
+		var currValidatingLine = vectors[j];
+		var expected = parseInt(currValidatingLine[currValidatingLine.length - 1]);
+		currValidatingLine = currValidatingLine[0];
+
+		if(expected * calculateKernel(currValidatingLine, hyperplane, p) <= 0) {
+			error++;
+		}
+	}
+
+	console.log('Tested Classifier with ' + ((error/vectors.length)*100) + '% error (' + error + '/' + vectors.length + ')');
+}
